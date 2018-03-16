6d6cd18db3d77932eb33087c66d3495797e453cd
iliran11 <iliran11@gmail.com>
2018-03-16 10:26:57 +0200

    appBar, scoreboard and progress bar.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import 'normalize.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import GameContainer from './components/gameContainer';
+import GameContainer from './components/gameContainer.jsx';
 import AppBar from 'material-ui/AppBar';
 import 'animate.css';
 import './App.css';
+import {appBarStyle} from './styles'
 
 
 class App extends Component {
@@ -13,7 +14,7 @@ class App extends Component {
       <MuiThemeProvider>
         <React.Fragment>
 
-          <AppBar title="Title" iconClassNameRight="muidocs-icon-navigation-expand-more" />
+          <AppBar style={appBarStyle} title="Title" iconClassNameRight="muidocs-icon-navigation-expand-more" className="app-bar"/>
           <GameContainer />
         </React.Fragment>
       </MuiThemeProvider>
