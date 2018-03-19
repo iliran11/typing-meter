4c287811a39b9ecb30970883ce75481a768e5e11
LiranCohen <iliran11@gmail.com>
2018-03-19 21:10:11 +0200

    progressbar start on game active

--- src/components/progress-bar.jsx
+++ src/components/progress-bar.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function ProgressBar() {
+export default function ProgressBar(props) {
+  const {isProgressCounting} = props
+  const animationClass = isProgressCounting ? 'progress-bar--animate ' : ''
   return (
-    <div className="progress-bar"></div>
+    <div className={`progress-bar ${animationClass}`}></div>
   )
 }
