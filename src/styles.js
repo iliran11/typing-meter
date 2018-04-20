4ff42806f47a8df3185b370a2e702347d25d7ad3
iliran11 <iliran11@gmail.com>
2018-04-20 12:58:40 +0300

    completion-modal - css refactor

--- src/styles.js
+++ src/styles.js
@@ -4,30 +4,21 @@ export const appBarStyle = {
 
 export const secondaryStyle = {
   backgroundColor: null,
-  height:null,
+  height: null,
   padding: null,
-  justifyContent:null
+  justifyContent: null
 };
 
 export const checkCircleStyle = {
-  width: '15vw',
-  height: '15vw',
-  color: 'white'
-}
+  width: null,
+  height: null,
+  color: null
+};
 export const svgStyle = {
-  color: '#004c40',
-  height:'initial'
-}
-export const completionModalSvg  = {
-  color: '#004c40'
-}
-export const completionModalTitle = {
-  color: '#004c40'
-}
-export const completionModalScore = {
-  color: '#004c40'
-}
+  color: null,
+  height: 'initial'
+};
 
 export const fontReset = {
   fontSize: 'inherit'
-}
+};
