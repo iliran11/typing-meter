86217e622af400f0e0019a218c9dffe4f3746d85
iliran11 <iliran11@gmail.com>
2018-05-20 01:59:56 +0300

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker container run \
 --env AWS_ACCESS_KEY_ID=AKIAIEB3UWNZPAKRKLOQ \
 --env AWS_SECRET_ACCESS_KEY=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 \
--v $PWD:/data \
+-v $PWD/build:/data \
 garland/aws-cli-docker \
 aws s3 sync . s3://www.typing-coacher.net --delete
