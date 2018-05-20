e53546c983e316ca2f8420020150f953e7c8556b
iliran11 <iliran11@gmail.com>
2018-05-20 12:49:29 +0300

    aws sync with keys

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker container run \
---env AWS_ACCESS_KEY_ID=AWS_ACCESS_KEY_ID \
---env AWS_SECRET_ACCESS_KEY=AWS_SECRET_ACCESS_KEY  \
+--env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+--env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY  \
 -v $PWD/build:/data \
 garland/aws-cli-docker \
-echo $AWS_ACCESS_KEY_ID
+aws s3 sync . s3://www.typing-coacher.net --delete
