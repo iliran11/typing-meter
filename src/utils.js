6655d76f2b78637a81ce92a8be8d0262ce6daea1
Liran Cohen <iliran11@gmail.com>
2018-05-25 01:53:39 +0300

    fixed a bug with detecting a space (#24)

--- src/utils.js
+++ src/utils.js
@@ -86,8 +86,8 @@ export function replaceLineBreaks(string) {
   return string.replace(/(?:\r\n|\r|\n)/g, ' ');
 }
 export function isLastCharIsSpace(str) {
-  /**https://stackoverflow.com/questions/36701001/check-if-last-typed-character-was-space?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa */
-  return str.substr(str.length - 1, str.length - 2) === ' ';
+ // https://stackoverflow.com/questions/3884632/how-to-get-the-last-character-of-a-string?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa  return str.substr(str.length - 1, str.length - 2) === ' ';
+  return str.slice(-1) === " "
 }
 export const noop = () => {};
 
