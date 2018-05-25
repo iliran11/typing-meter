ae1b1164e37086a102b1efa58271367f225c8bc5
iliran11 <iliran11@gmail.com>
2018-05-26 01:37:25 +0300

    words list instead of wordboard

---
+++ src/components/game-container/WordsList.jsx
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react';
+import Word from './Word2';
+
+export default function WordsList(props) {
+  const { words, currentGamePosition,isActive } = props;
+  const disableClass = isActive ? '' : 'disabled'
+  const wordsBoardClasses = `words-container size1 transitionable ${disableClass}`
+  const renderWords = (word, index) => {
+    const isActive = index === currentGamePosition;
+    return (
+      <Fragment key={word.key}>
+        <Word
+          displayedLetters={word.challenge.split('')}
+          typedLetters={word.typed.split('')}
+          isCompleted={word.isCompleted}
+          isCorrect={word.isCorrect}
+          isActive={isActive}
+        />{' '}
+      </Fragment>
+    );
+  };
+  return <div className={wordsBoardClasses}>{words.map(renderWords)}</div>;
+}
