f618278a54894113950dbd56188ed1962e9b9cac
iliran11 <iliran11@gmail.com>
2018-05-26 01:37:07 +0300

    new word2 file. init refactor

---
+++ src/components/game-container/Word2.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Letter from './Letter.jsx';
+import scrollIntoView from 'scroll-into-view';
+import { FAILURE_ANIMATION, SUCCESS_ANIMATION } from '../../constants';
+import LettersList from './LettersList';
+
+export default class Word extends Component {
+  get isWordCorrectClass() {
+    const { isCompleted, isCorrect } = this.props;
+    /** we can assume if word is correct or not -
+     *  only if the word is correct.
+     */
+    if (isCompleted) {
+      return isCorrect ? 'correct-word' : 'incorrect-word';
+    }
+    return '';
+  }
+  get activeClass() {
+    return this.props.isActive ? 'active' : '';
+  }
+  get animationClass() {
+    const { isCompleted, isCorrect } = this.props;
+    if (isCompleted) {
+      return isCorrect ? SUCCESS_ANIMATION : FAILURE_ANIMATION;
+    }
+    return '';
+  }
+  get containerClasses() {
+    return `${this.isWordCorrectClass} ${this.activeClass}`;
+  }
+  get letters() {
+    return 'hello world';
+  }
+  render() {
+    return (
+      <span className={this.containerClasses} tabIndex="1">
+        <LettersList
+          displayedLetters={this.props.displayedLetters}
+          typedLetters={this.props.typedLetters}
+        />
+      </span>
+    );
+  }
+}
