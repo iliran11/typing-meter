0e71c7ef6a64b00df8ac2fe02ce959d9a4ff8062
iliran11 <iliran11@gmail.com>
2018-05-31 00:18:03 +0300

    cleanup

--- src/App.js
+++ src/App.js
@@ -5,29 +5,8 @@ import GameContainer from './components/game-container/gameContainer2';
 import AppBar from './components/appbar2';
 import 'animate.css';
 import './App.css';
-import CompletionModal from './components/completionModal';
-import {
-  AWAITS_TYPING,
-  GAME_IS_ACTIVE,
-  RESTART_PENDING,
-  GAME_DURATION
-} from './constants';
-import Settings from './components/settings';
-import Dialog from 'material-ui/Dialog';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gameStatus: AWAITS_TYPING,
-      settingsOpen: false,
-      customWords: null,
-      gameDuration: GAME_DURATION
-    };
-    this.isGameFinished = false;
-    this.correctTypedWords = 0;
-    this.wpm = 0;
-  }
   render() {
     return (
       <MuiThemeProvider>
