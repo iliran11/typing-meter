ed8a0df5d5f0b52b8f476570658e4818b44e0473
iliran11 <iliran11@gmail.com>
2018-05-31 01:37:30 +0300

    router implementation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import 'normalize.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import GameContainer from './components/game-container/gameContainer2';
 import AppBar from './components/appbar2';
+import Settings from './components/settings';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import 'animate.css';
 import './App.css';
 
@@ -10,10 +12,13 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <React.Fragment>
-          <AppBar onSettingsClick={this.toggleSettings} />
-          <GameContainer />
-        </React.Fragment>
+        <Router>
+          <Fragment>
+            <AppBar onSettingsClick={this.toggleSettings} />
+            <Route exact path="/" component={GameContainer} />
+            <Route exact path="/settings" component={Settings} />
+          </Fragment>
+        </Router>
       </MuiThemeProvider>
     );
   }
--- src/components/appbar2.jsx
+++ src/components/appbar2.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { noop } from '../utils';
+import { Link } from 'react-router-dom';
 
 export default function AppBar(props) {
   const { onSettingsClick = noop } = props;
   return (
     <div className="app-bar">
       <div className="left-hand">
-        <i className="fab fa-angellist logo" />
+        <Link to="/">
+          <i className="fab fa-angellist logo" />
+        </Link>
       </div>
 
       <div className="right-hand">
-        <i className="fas fa-cog" onClick={onSettingsClick} />
+        <Link to="/settings">
+          <i className="fas fa-cog" />
+        </Link>
         <i className="fab fab fa-github github" />
       </div>
     </div>
