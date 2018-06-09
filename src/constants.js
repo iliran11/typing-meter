d8b55795117bdabc6c38a492e2cc8f22b3660604
iliran11 <iliran11@gmail.com>
2018-06-09 10:59:40 +0300

    revert constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const WPM_NULL = '0';
-export const GAME_DURATION = 3;
+export const GAME_DURATION = 60;
 export const METRICS_INTERVAL_DELAY = 500;
 export const DEBUG_MODE = false;
 export const FAILURE_ANIMATION = `animated jello`;
