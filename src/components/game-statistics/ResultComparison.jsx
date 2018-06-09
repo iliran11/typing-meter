18f7e230e92157fb920812bff572cdab47e78d82
iliran11 <iliran11@gmail.com>
2018-06-09 20:58:28 +0300

    cleanups

--- src/components/game-statistics/ResultComparison.jsx
+++ src/components/game-statistics/ResultComparison.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import gameStats from './gameStats'
+// import gameStats from './gameStats'
 
 export default function ResultComparison(props) {
-  const {wpmResult} = props
-  const percentile = gameStats(wpmResult);
+  // const {wpmResult} = props
+  // const percentile = gameStats(wpmResult);
 
   return (
     <div className="result-comparison">hello</div>
