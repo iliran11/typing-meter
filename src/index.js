8550242b215e99708295e3c0a6303e092ce17eae
iliran11 <iliran11@gmail.com>
2018-06-14 01:31:47 +0300

    redux-wiring

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/store';
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Index = () => (
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>
+);
+ReactDOM.render(<Index />, document.getElementById('root'));
 registerServiceWorker();
