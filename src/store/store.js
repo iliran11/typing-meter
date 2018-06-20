4f103f5d3667dff0099c4a999651cb5933c4104a
Liran Cohen <iliran11@gmail.com>
2018-06-20 16:21:22 +0300

    cleanups

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,8 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 export default function configureStore() {
   return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-}
-
-function getReduxDevTools() {
-  if (typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "function") {
-    return window.__REDUX_DEVTOOLS_EXTENSION__();
-  }
-  return undefined;
 }
