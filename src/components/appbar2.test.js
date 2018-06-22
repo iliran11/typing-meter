7aeabf3c9bce76ddced6fae32f1f4c31e55bf87b
iliran11 <iliran11@gmail.com>
2018-06-22 22:24:34 +0300

    app-bar testing

---
+++ src/components/appbar2.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import AppBar from './appbar2';
+import { shallow } from 'enzyme';
+
+function AppBarFactory(walkThroughIconStatus) {
+  return shallow(<AppBar walkThroughIconStatus={walkThroughIconStatus} />);
+}
+function getInfoIconStyle(walkThroughIconStatus) {
+  const wrapper = AppBarFactory(walkThroughIconStatus);
+  return wrapper.find('.fa-info-circle').prop('style');
+}
+describe('`AppBar`', () => {
+  it('renders properly', () => {
+    const wrapper = AppBarFactory();
+    expect(wrapper).toMatchSnapshot();
+  });
+  describe(`help icon`, () => {
+    it('is greyed out when icon status is false', () => {
+      const informationIconStyle = getInfoIconStyle(false);
+      expect(informationIconStyle).toHaveProperty('opacity', 0.5);
+    });
+    it('is not greyed when icon status is true', () => {
+      const informationIconStyle = getInfoIconStyle(true);
+      expect(informationIconStyle).toEqual({});
+    });
+  });
+});
