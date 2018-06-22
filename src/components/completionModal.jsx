d263d9c71f70a733f62405d500e2115449e763be
iliran11 <iliran11@gmail.com>
2018-06-23 01:00:38 +0300

    completion modal test

--- src/components/completionModal.jsx
+++ src/components/completionModal.jsx
@@ -4,9 +4,10 @@ import ChceckCircle from 'material-ui/svg-icons/action/check-circle';
 import AutoRenew from 'material-ui/svg-icons/action/autorenew';
 import { checkCircleStyle } from '../styles';
 import ScoreSection from './scoreboard/score-section';
-import { svgStyle,  } from '../styles';
-import ResultComparison from './game-statistics/ResultComparison' 
-import gameStats from './game-statistics/gameStats'
+import { svgStyle } from '../styles';
+import ResultComparison from './game-statistics/ResultComparison';
+import gameStats from './game-statistics/gameStats';
+import { bool, number, func } from 'prop-types';
 
 const Title = function() {
   return (
@@ -17,14 +18,28 @@ const Title = function() {
 };
 
 export default function CompletionModal(props) {
-  const { modal = false, open = true, correctTypedWords, wpmScore, onRestart } = props;
+  const {
+    modal = false,
+    open = true,
+    correctTypedWords,
+    wpmScore,
+    onRestart
+  } = props;
   const TitleNode = Title();
-  const percentileResult = gameStats(wpmScore)
+  const percentileResult = gameStats(wpmScore);
   return (
-    <Dialog repositionOnUpdate={false} modal={modal} open={open} title={TitleNode} className="completion-dialog">
+    <Dialog
+      repositionOnUpdate={false}
+      modal={modal}
+      open={open}
+      title={TitleNode}
+      className="completion-dialog"
+    >
       <div id="completion-content">
-        <h4 className="size2" id="your-score">{`You type faster than ${percentileResult.percentile}% of others`}</h4>
-        <ResultComparison result={wpmScore}/>
+        <h4 className="size2" id="your-score">{`You type faster than ${
+          percentileResult.percentile
+        }% of others`}</h4>
+        <ResultComparison result={wpmScore} />
         <div className="score-report">
           <ScoreSection
             title="CORRECT"
@@ -47,3 +62,11 @@ export default function CompletionModal(props) {
     </Dialog>
   );
 }
+
+CompletionModal.propTypes = {
+  modal: bool,
+  repositionOnUpdate: bool,
+  correctTypedWords: number,
+  wpmScore: number,
+  onRestart: func
+};
