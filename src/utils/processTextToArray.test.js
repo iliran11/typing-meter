ed08843ee657a8986e154b3b3096edf17a119e62
iliran11 <iliran11@gmail.com>
2018-06-23 12:37:55 +0300

    function testing

---
+++ src/utils/processTextToArray.test.js
@@ -0,0 +1,32 @@
+import {processTextToArray} from './utils'
+
+describe('`processTextToArray`', () => {
+  test('gets 3 normal words', () => {
+    const textSample = `hello bye ffff`
+    expect(processTextToArray(textSample)).toMatchSnapshot();
+  });
+  test('gets empty string', () => {
+    const textSample = ``
+    expect(processTextToArray(textSample)).toMatchSnapshot();
+  });
+  test('gets empty string with spaces', () => {
+    const textSample = `       `
+    expect(processTextToArray(textSample)).toMatchSnapshot();
+  });
+  test('gets single letter word', () => {
+    const textSample = `hello . hello`
+    expect(processTextToArray(textSample)).toMatchSnapshot();
+  });
+  test('gets padding text', () => {
+    const textSample = `     hello hello     `
+    expect(processTextToArray(textSample)).toMatchSnapshot();
+  });
+  test('gets line breaks', () => {
+    const textSample = `hello
+       bye 
+       
+       line breaks`
+    expect(processTextToArray(textSample)).toMatchSnapshot();
+  });
+
+})
