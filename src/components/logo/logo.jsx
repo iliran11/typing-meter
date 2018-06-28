a24699e618b70c3019fd1ac1365513603c1b9838
Liran Cohen <iliran11@gmail.com>
2018-06-28 20:35:01 +0300

    logo will break on desktop too

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import keyboard from './keyboard.svg';
 
 export default function Logo() {
-  const isMobile = window.innerWidth < 480;
-  const breakPoint = isMobile ? <br /> : null;
+  const isMobile = true;
   const spanMarginTop = isMobile ? 4 : 0;
   return (
     <div className="logo">
       <img alt={'logo'} src={keyboard} />
       <span style={{ marginTop: spanMarginTop }}>
-        TYPING {breakPoint}COACHER
+        TYPING <br/> COACHER
       </span>
     </div>
   );
