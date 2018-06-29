726f4bdabce36e62c2c607e6a7c4d62ea6a82c74
iliran11 <iliran11@gmail.com>
2018-06-29 12:53:04 +0300

    updated tests

--- src/components/scoreboard/score-section.test.js
+++ src/components/scoreboard/score-section.test.js
@@ -5,7 +5,8 @@ import { shallow } from 'enzyme';
 const props = {
   title: 'WPM',
   score: 20,
-  disabled:true 
+  disabled:true,
+  specialScoreClass: 'special-class'
 }
 const wrapper = shallow(<ScoreSection {...props} />);
 
