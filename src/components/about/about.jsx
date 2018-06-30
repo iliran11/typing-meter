aa6c017666b93019d44fb546b0dcb477a4b7777e
iliran11 <iliran11@gmail.com>
2018-07-01 01:16:07 +0300

    additions to about me

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,14 +3,20 @@ import Avatar from 'material-ui/Avatar';
 import Divider from 'material-ui/Divider';
 import ListItem from './ListItem';
 import aboutMeList from './aboutMeList';
+import openProjectList from './openProjectList';
 import contact from './assets/contact.svg';
+import facebook from './assets/facebook.svg';
+import linkedin from './assets/linkedin.svg';
+import github from './assets/github.svg';
+import bug from './assets/bug.svg';
+import openSource from './assets/open-source.svg';
 
 export default function AboutMe(props) {
   return (
     <div id="about-me">
       <section>
         <h1>
-          <Avatar src={'./liran.jpg'} size={100} /> Hi, My name is Liran.
+          <Avatar src={'./liran.jpg'} style={{height:null,width:null}} /> Hi, My name is Liran.
         </h1>
         <Divider />
         <p>
@@ -30,13 +36,34 @@ export default function AboutMe(props) {
         <h1>
           <img src={contact} alt={'contact me'} /> How to Reach Me
         </h1>
-        <Divider />
         <p>
           It would be my pleasure to be contact and discuss anything. from
           typing fast topics, feature requests to other projects endevours.
           Please look out for me in the following platforms:
         </p>
+        <div className="social-grid">
+          <img src={facebook} className="social-item" />
+          <img src={linkedin} className="social-item" />
+          <img src={github} className="social-item" />
+        </div>
       </section>
+      <Divider style={{ height: 3 }} />
+      <section>
+        <h1>
+          <img src={openSource} alt={'bugs reports'} />Open Source Porject
+        </h1>
+        <Divider />
+        <p>
+          This Project is hosted on github as Open Source. So you can read the
+          code and even contribute. or fix bugs.
+        </p>
+        <ul>
+          {openProjectList.map(item => {
+            return <ListItem icon={item.img}>{item.text}</ListItem>;
+          })}
+        </ul>
+      </section>
+
       {/* <video
         loop
         autoPlay
---
+++ src/components/about/openProjectList.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import { Link } from 'react-router-dom';
+import openSource from './assets/open-source.svg'
+import bug from './assets/bug.svg'
+
+export default [
+  {
+    text: <Link to="https://github.com/iliran11/typing-meter">Read The Code</Link>,
+    img: openSource
+  },
+  {
+    text: <Link to="https://github.com/iliran11/typing-meter/issues">Report a bug</Link>,
+    img: bug
+  }
+];
