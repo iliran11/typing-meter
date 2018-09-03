71250429ac1f79346ed670e8f0628ad099627e6b
iliran11 <iliran11@gmail.com>
2018-09-03 19:59:53 +0300

    result page styling

--- src/pages/result/ResultPage.jsx
+++ src/pages/result/ResultPage.jsx
@@ -10,46 +10,52 @@ import {
 import { number, func } from 'prop-types';
 import speedometer from '../../components/speedometer.svg';
 import correctIcon from './correct.svg';
+import history from '../../history'
 
 const Title = function(props) {
   return (
-    <h1>
+    <div className="full-width background-primary text-center">
       <img
         alt={'Shows emotions about the result'}
         src={delightPicture(props.wpm)}
         className="delight-complete"
       />
-    </h1>
+    </div>
   );
 };
-
+function onRestart() {
+  history.push(`/`);
+}
 export default function ResultPage(props) {
-  const { correctTypedWords, wpmScore, onRestart } = props;
+  const { correctTypedWords, wpmScore } = props;
+
   return (
     <div id="completion-content">
-      <Title wpm={wpmScore}/>
-      <h4 className="size2" id="your-score">
-        {resultDescription(wpmScore)}
-      </h4>
-      <ResultComparison result={wpmScore} />
-      <div className="score-report">
-        <ScoreSection
-          title="CORRECT"
-          iconClass="fas fa-check-circle"
-          score={correctTypedWords}
-          icon={correctIcon}
-        />
-        <ScoreSection
-          title="WPM"
-          iconClass="fas fas fa-tachometer-alt"
-          score={wpmScore}
-          icon={speedometer}
-        />
-      </div>
-      <h4 className="size2">Try Again?</h4>
-      <div>
-        <div className="svg-wrapper svg-size1 restart" onClick={onRestart}>
-          <AutoRenew style={svgStyle} />
+      <Title wpm={wpmScore} />
+      <div className='full-width' style={{padding:'0px 20px'}}>
+        <h4 className="size2" id="your-score">
+          {resultDescription(wpmScore)}
+        </h4>
+        <ResultComparison result={wpmScore} />
+        <div className="score-report">
+          <ScoreSection
+            title="CORRECT"
+            iconClass="fas fa-check-circle"
+            score={correctTypedWords}
+            icon={correctIcon}
+          />
+          <ScoreSection
+            title="WPM"
+            iconClass="fas fas fa-tachometer-alt"
+            score={wpmScore}
+            icon={speedometer}
+          />
+        </div>
+        <h4 className="size2 text-center">Try Again?</h4>
+        <div className="restart">
+          <div className="svg-wrapper svg-size1" onClick={onRestart}>
+            <AutoRenew style={svgStyle}/>
+          </div>
         </div>
       </div>
     </div>
