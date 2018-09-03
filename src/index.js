ba9b03c24d5d846de37b877994c3452f0bae0a0f
iliran11 <iliran11@gmail.com>
2018-09-04 00:23:44 +0300

    entry point refactoring

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/store';
-import { Provider } from 'react-redux';
-import { loadState, saveState } from './store/persistor';
+import Root from './Root';
 
-const persistedState = loadState()
-const store = configureStore(persistedState);
-store.subscribe(() => {
-  saveState(store.getState());
-});
+const store = configureStore();
 
-const Index = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
 registerServiceWorker();
